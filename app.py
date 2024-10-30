50fc1bac0c0d7cea4f29f8cb4efa6283b876abfe
cayel <christophe.ayel@gmail.com>
2024-10-30 14:25:42 +0100

    add mock system

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import streamlit as st
 import requests
+from mock_data import mock_fetch_artists, mock_fetch_releases
 
 API_URL = "https://moleskine-api.vercel.app/"
 
+    
+class Artist:
+    def __init__(self, artist_name, artist_id):
+        self.name = artist_name
+        self.id = artist_id
+        
+class Release:
+    def __init__(self, release_artist, release_title, release_date):
+        self.title = release_title
+        self.date = release_date
+        self.artist = release_artist
+        
+def fetch_mock_artists():
+    return [Artist(artist['name'], artist['id']) for artist in mock_fetch_artists()]
+    
+def fetch_mock_releases():
+    return [Release(release['artist'], release['title'], release['date']) for release in mock_fetch_releases()]
+        
 def fetch_artists(api_url):
     response = requests.get(f"{api_url}artist")
     if response.status_code == 200:
@@ -23,17 +42,17 @@ def main():
     st.title("Moleskine-V2")
     st.write("Bienvenue dans Moleskine !")
 
-    artists = fetch_artists(API_URL)
-    artist_names = [artist['artist_name'] for artist in artists]
+    artists = fetch_mock_artists()
+    artist_names = [artist.name for artist in artists]
 
     if artist_names:
         selected_artist = st.selectbox("Sélectionnez un artiste", artist_names)
         st.write(f"Vous avez sélectionné : {selected_artist}")
         
-    releases = fetch_releases(API_URL)
+    releases = fetch_mock_releases()
 
     if releases:
-        release_data = [(release['release_title'], release['release_date']) for release in releases]
+        release_data = [(release.title, release.date) for release in releases]
         
         # Créer un tableau HTML
         html_table = """
